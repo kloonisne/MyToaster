7d7f1e1bee10e19979f4508bdad9f67332e386bd
Akshay Kumar <mail2akshaykumarpv@gmail.com>
2021-05-19 19:04:39 +0530

    customising toast

--- app/src/main/java/com/task/mylibraryinkotlin/MainActivity.kt
+++ app/src/main/java/com/task/mylibraryinkotlin/MainActivity.kt
@@ -2,11 +2,17 @@ package com.task.mylibraryinkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.task.mylibrary.Toaster
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val txtHello = findViewById<TextView>(R.id.txtHello)
+        txtHello.setOnClickListener {
+            Toaster.show(this,"Yes I am working!!!!")
+        }
     }
 }
--- mylibrary/src/main/java/com/task/mylibrary/Toaster.kt
+++ mylibrary/src/main/java/com/task/mylibrary/Toaster.kt
@@ -1,12 +1,27 @@
 package com.task.mylibrary
 
+import android.app.Activity
 import android.content.Context
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
+
 
 class Toaster {
     companion object{
-        fun show(ctx:Context, msg:String){
-            Toast.makeText(ctx,msg,Toast.LENGTH_SHORT).show();
+        fun show(ctx: Activity, msg: String){
+            val inflater: LayoutInflater = LayoutInflater.from(ctx)
+            val view:View = inflater.inflate(R.layout.item_toast, (ctx).findViewById(R.id.toastRoot))
+            val txtMsg:TextView = view.findViewById(R.id.toastMessage)
+            txtMsg.text = msg
+            val toast = Toast(ctx)
+            toast.setGravity(Gravity.TOP,0,0)
+            toast.duration = Toast.LENGTH_LONG
+            toast.view =view
+            toast.show()
         }
     }
 }
